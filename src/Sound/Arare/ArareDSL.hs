c0c9831ea599979307ef6a0e08c74e54fa3f5466
Tokiwo Ousaka <its.out.of.tune.this.my.music@gmail.com>
2015-03-09 19:37:34 +0900

    add ScoreMonad

--- src/Sound/Arare/ArareDSL.hs
+++ src/Sound/Arare/ArareDSL.hs
@@ -7,17 +7,14 @@ import Control.Monad.Operational
 import Control.Monad.State
 
 --------
--- ArareSequencer : 旋律を記述する
-
-data Note
-data SequenceTime
+-- ArareSequencer : トラックを記述する
 
 class NoteController n where
-  noteOn :: SequenceTime -> Note -> n
-  noteOff :: SequenceTime -> Note -> n
+  noteOn :: Note -> n
+  noteOff :: Note -> n
 
 class BendController b where
-  bendKey :: SequenceTime -> Note -> b
+  bendKey :: Note -> b
 
 class (NoteController i, BendController i) => Instrument i where
 
@@ -25,6 +22,7 @@ data ArareSequencerBase o i a where
   GetInstrumentSequenceTime :: ArareSequencerBase o i SequenceTime
   PutInstrumentSequenceTime :: SequenceTime -> ArareSequencerBase o i ()
   SendInstrumentMessage :: i -> ArareSequencerBase o i ()
+  ArareFromScore :: Score () -> ArareSequencerBase o i ()
 
 ----
 
@@ -39,18 +37,12 @@ putInstrumentSequenceTime = singleton . PutInstrumentSequenceTime
 sendInstrumentMessage :: i -> ArareSequencer i ()
 sendInstrumentMessage = singleton . SendInstrumentMessage
 
+score :: Score () -> ArareSequencer i ()
+score = singleton . ArareFromScore
+
 instance MonadState SequenceTime (ArareSequencer i) where
   get = getInstrumentSequenceTime
   put = putInstrumentSequenceTime  
-
---------
--- ArareRhythmBox : リズムパートを記述する
-
-class Percussion p where
-
-data ArareRhythmBoxBase o p a where
-  GetPercussionSequenceTime :: ArareRhythmBoxBase o p SequenceTime
-  SendParcussionMessage :: p -> ArareRhythmBoxBase o i ()
 
 --------
 -- ArareWaveBase : 波形データを直接操作する
@@ -62,4 +54,72 @@ data ArareWaveBase where
 
 data ArareMixer where
 
+--------
+-- Sequencable : シーケンサ本体
+
+data NoteScale
+data Note = Note
+  { noteScale :: NoteScale
+  , noteVelocity :: Float
+  } 
+data SequenceTime
+type NoteLength = Int
+
+data ScoreBase a where
+  WriteNote :: Note -> NoteLength -> ScoreBase ()
+  GetSequenceTime :: ScoreBase SequenceTime
+  PutSequenceTime :: SequenceTime -> ScoreBase ()
+
+type Score = Program ScoreBase 
+
+writeNote :: Note -> NoteLength -> Score ()
+writeNote n = singleton . WriteNote n
+
+getSequenceTime :: Score SequenceTime
+getSequenceTime = singleton $ GetSequenceTime
+
+petSequenceTime :: SequenceTime -> Score ()
+petSequenceTime = singleton . PutSequenceTime
+
+--------
+
+c :: Int -> Float -> Score ()
+c = undefined
+
+d :: Int -> Float -> Score ()
+d = undefined
+
+e :: Int -> Float -> Score ()
+e = undefined
+
+f :: Int -> Float -> Score ()
+f = undefined
+
+g :: Int -> Float -> Score ()
+g = undefined
+
+--------
+
+class Sequencable s where
+
+instance Sequencable (Float -> Score ()) where
+instance Sequencable (Int -> Float -> Score ()) where
+instance Sequencable (Score ()) where
+
+infixl 9 ==>
+(==>) :: (Sequencable a, Sequencable b) => a -> b -> Score ()
+(==>) = undefined
+
+--------
+-- test code
+
+data SinOscillator
+
+test :: ArareSequencer i ()
+test = do
+  score $ do 
+   -- さ〜い〜た〜さ〜い〜た〜
+   c ==> d ==> e 2 ==> c ==> d ==> e 2
+   -- ちゅ〜りっぷ〜の〜は〜な〜が〜
+   g ==> e ==> d ==> c ==> d ==> e ==> d 2
 
